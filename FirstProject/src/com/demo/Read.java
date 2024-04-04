300bc5547e057e40e966cf5f800f691581c430d0
ShwetaSarnaiks <shwetasarnaik2@gmail.com>
2024-04-04 12:18:16 +0530

    first comment

---
+++ FirstProject/src/com/demo/Read.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class Read {
+
+	public static void main(String[] args) {
+		System.out.println("one");
+
+	}
+
+}
---
+++ FirstProject/src/com/demo/Write.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+public class Write {
+
+	public static void main(String[] args) {
+     System.out.println("get");
+	}
+
+}
